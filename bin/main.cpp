0d91408045b6ca7f60a135b4718169db798d64b7
Danil Abdulminev <danil.abdulminev@mail.ru>
2024-03-19 02:51:05 +0300

    сделано что-то осознаное

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,15 +4,12 @@
 
 // Протестировать свои функции
 // Написать тесты
-// написать minimum и maximum
-// подумать над swap
 // подумать все ли варианты методов написаны
-// сделать нормальный tag-dispathing 
 
 int main() {
-  // bst<int, InOrder, std::greater<int>> a, c;
-  //   bst<int, InOrder> b = a;
-  //   c = a;
-  //   std::cout << b.max_size() << ' ' << c.size();
+  bst<int, InOrder, std::greater<int>> a, c;
+  bst<int, InOrder , std::greater<int>> b = a;
+  c = a;
+  std::cout << b.max_size() << ' ' << c.size();
   return 0;
 }
--- lib/bst.h
+++ lib/bst.h
@@ -4,298 +4,135 @@
 #include <memory>
 #include <vector>
 
-struct PreOrder {};
-struct InOrder {};
-struct PostOrder {};
+enum Order { InOrder, PostOrder, PreOrder };
 
-template <class T, class Traversal>
-struct Iterator {};
-
-template <class T>
-class Iterator<T, InOrder> {
-  typedef T value_type;
-  typedef Iterator<T, InOrder> node_type;
-  typedef node_type& reference;
-  typedef const node_type& const_reference;
-  typedef node_type* iterator;
-  typedef const node_type* const_iterator;
-  typedef size_t size_type;
-
-  value_type value;
-  iterator left;
-  iterator right;
-  iterator parent;
-  std::vector<iterator> iterators_order;
-  size_t size;
-  size_t it;
-
-  void inorder(iterator node) {
-    if (node == nullptr) {
-      return;
-    }
-    inorder(node->left, iterators_order);
-    iterators_order.push_back(node->value);
-    inorder(node->right, iterators_order);
-  }
-
-  Iterator() {
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-
-  Iterator(value_type& data) {
-    value = data;
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-
-  Iterator(size_t size, iterator root) : size{size} {
-    inorder(root);
-    it = 0;
-  }
-
-  iterator operator++() {
-    if (it < size) {
-      return iterators_order[++it];
-    } else {
-      return iterators_order[size - 1]->left;
-    }
-  }
-
-  iterator operator--() {
-    if (it >= size) {
-      return iterators_order[--it];
-    } else {
-      return iterators_order[0];
-    }
-  }
-
-  iterator operator++(int) {
-    if (it >= size) {
-      return iterators_order[size - 1]->left;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it < size) {
-      ++it;
-    }
-    return copy_iteartor;
-  }
-
-  iterator operator--(int) {
-    if (it < 0) {
-      return nullptr;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it >= size) {
-      --it;
-    }
-    return copy_iteartor;
-  }
-
-  reference operator*() { return &iterators_order[it]; }
-
-  bool operator==(const_reference other) {
-    return (this->value == other.value) && (this->left == other.left) &&
-           (this->right == other.right) && (this->parent == other.parent);
-  }
-
-  bool operator!=(const_reference other) { return !(this == other); }
-};
-template <typename T>
-struct Iterator<T, PostOrder> {
-  typedef T value_type;
-  typedef Iterator<T, InOrder> node_type;
-  typedef node_type& reference;
-  typedef const node_type& const_reference;
-  typedef node_type* iterator;
-  typedef const node_type* const_iterator;
-  typedef size_t size_type;
-
-  value_type value;
-  iterator left;
-  iterator right;
-  iterator parent;
-  std::vector<iterator> iterators_order;
-  size_t size;
-  size_t it;
-
-  void postorder(iterator node) {
-    if (node == nullptr) {
-      return;
-    }
-    postorder(node->left, iterators_order);
-    postorder(node->right, iterators_order);
-    iterators_order.push_back(node->value);
-  }
-
-  Iterator() {
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-  Iterator(value_type& data) {
-    value = data;
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-
-  Iterator(size_t size, iterator root) : size{size} {
-    postorder(root);
-    it = 0;
-  }
-
-  iterator operator++() {
-    if (it < size) {
-      return iterators_order[++it];
-    } else {
-      return iterators_order[size - 1]->left;
-    }
-  }
-
-  iterator operator--() {
-    if (it >= size) {
-      return iterators_order[--it];
-    } else {
-      return iterators_order[0];
-    }
-  }
-
-  iterator operator++(int) {
-    if (it >= size) {
-      return iterators_order[size - 1]->left;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it < size) {
-      ++it;
-    }
-    return copy_iteartor;
-  }
-
-  iterator operator--(int) {
-    if (it < 0) {
-      return nullptr;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it >= size) {
-      --it;
-    }
-    return copy_iteartor;
-  }
-
-  reference operator*() { return &iterators_order[it]; }
-
-  bool operator==(const_reference other) {
-    return (this->value == other.value) && (this->left == other.left) &&
-           (this->right == other.right) && (this->parent == other.parent);
-  }
-
-  bool operator!=(const_reference other) { return !(this == other); }
-};
-
-template <class T>
-struct Iterator<T, PreOrder> {
-  typedef T value_type;
-  typedef Iterator<T, InOrder> node_type;
-  typedef node_type& reference;
-  typedef const node_type& const_reference;
-  typedef node_type* iterator;
-  typedef const node_type* const_iterator;
-  typedef size_t size_type;
-
-  value_type value;
-  iterator left;
-  iterator right;
-  iterator parent;
-  std::vector<iterator> iterators_order;
-  size_t size;
-  size_t it;
-
-  void preorder(iterator node) {
-    if (node == nullptr) {
-      return;
-    }
-    iterators_order.push_back(node->value);
-    preorder(node->left, iterators_order);
-    preorder(node->right, iterators_order);
-  }
-
-  Iterator() {
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-  Iterator(value_type& data) {
-    value = data;
-    left = nullptr;
-    right = nullptr;
-    parent = nullptr;
-  }
-
-  Iterator(size_t size, iterator root) : size{size} {
-    postorder(root);
-    it = 0;
-  }
-
-  iterator operator++() {
-    if (it < size) {
-      return iterators_order[++it];
-    } else {
-      return iterators_order[size - 1]->left;
-    }
-  }
-
-  iterator operator--() {
-    if (it >= size) {
-      return iterators_order[--it];
-    } else {
-      return iterators_order[0];
-    }
-  }
-
-  iterator operator++(int) {
-    if (it >= size) {
-      return iterators_order[size - 1]->left;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it < size) {
-      ++it;
-    }
-    return copy_iteartor;
-  }
-
-  iterator operator--(int) {
-    if (it < 0) {
-      return nullptr;
-    }
-    iterator copy_iteartor = iterators_order[it];
-    if (it >= size) {
-      --it;
-    }
-    return copy_iteartor;
-  }
-
-  reference operator*() { return &iterators_order[it]; }
-
-  bool operator==(const_reference other) {
-    return (this->value == other.value) && (this->left == other.left) &&
-           (this->right == other.right) && (this->parent == other.parent);
-  }
-
-  bool operator!=(const_reference other) { return !(this == other); }
-};
-template <class T, class Traversal = InOrder, class Compare = std::less<T>,
+template <class T, Order order = InOrder, class Compare = std::less<T>,
           class Allocator = std::allocator<T> >
 class bst {
+template <class U , Order Traversal>
+  struct Node {
+    typedef U value_type;
+    typedef Node<T, Traversal> node_type;
+    typedef node_type& reference;
+    typedef const node_type& const_reference;
+    typedef node_type* iterator;
+    typedef const node_type* const_iterator;
+    typedef size_t size_type;
+
+    value_type value;
+    iterator left;
+    iterator right;
+    iterator parent;
+    std::vector<iterator> iterators_order;
+    size_t size;
+    size_t it;
+
+    void inorder(iterator node) {
+      if (node == nullptr) {
+        return;
+      }
+      inorder(node->left);
+      iterators_order.push_back(node);
+      inorder(node->right);
+    }
+
+    void postorder(iterator node) {
+      if (node == nullptr) {
+        return;
+      }
+      postorder(node->left, iterators_order);
+      postorder(node->right, iterators_order);
+      iterators_order.push_back(node);
+    }
+
+    void preorder(iterator node) {
+      if (node == nullptr) {
+        return;
+      }
+      iterators_order.push_back(node);
+      preorder(node->left);
+      preorder(node->right);
+    }
+
+    Node() {
+      left = nullptr;
+      right = nullptr;
+      parent = nullptr;
+    }
+
+    Node(value_type& data) {
+      value = data;
+      left = nullptr;
+      right = nullptr;
+      parent = nullptr;
+    }
+
+    void initiliaze(size_t size, iterator root) {
+      this->size = size;
+      it = 0;
+      if constexpr (Traversal == InOrder) {
+        inorder(root);
+      } else if constexpr (Traversal == PostOrder) {
+        postorder(root);
+      } else if constexpr (Traversal == PreOrder) {
+        preorder(root);
+      }
+    }
+
+    iterator operator++() {
+      if (it < size) {
+        return iterators_order[++it];
+      } else {
+        return iterators_order[size - 1];
+      }
+    }
+
+    iterator operator--() {
+      if (it >= size) {
+        return iterators_order[--it];
+      } else {
+        return iterators_order[0];
+      }
+    }
+
+    iterator operator++(int) {
+      if (it >= size) {
+        return iterators_order[size - 1];
+      }
+      iterator copy_iteartor = iterators_order[it];
+      if (it < size) {
+        ++it;
+      }
+      return copy_iteartor;
+    }
+
+    iterator operator--(int) {
+      if (it < 0) {
+        return nullptr;
+      }
+      iterator copy_iteartor = iterators_order[it];
+      if (it >= size) {
+        --it;
+      }
+      return copy_iteartor;
+    }
+
+    reference operator*() { return &iterators_order[it].value; }
+
+    bool operator==(const_reference other) {
+      return (this->value == other.value) && (this->left == other.left) &&
+             (this->right == other.right) && (this->parent == other.parent);
+    }
+
+    bool operator!=(const_reference other) { return !(this == other); }
+  };
+
  private:
   typedef T value_type;
-  typedef Iterator<value_type, Traversal> node_type;
-  typedef node_type& reference;
-  typedef const node_type& const_reference;
-  typedef node_type* iterator;
-  typedef const node_type* const_iterator;
+  typedef Node<value_type, order>* iterator;
+  typedef bst& reference;
+  typedef const bst& const_reference;
+  typedef const iterator const_iterator;
   typedef size_t size_type;
   typedef Allocator allocator_type;
 
@@ -313,7 +150,7 @@ class bst {
     if (node == nullptr) {
       return;
     }
-    iterators_order.push_back(node->data);
+    iterators_order.push_back(node);
     preorder(node->left, iterators_order);
     preorder(node->right, iterators_order);
   }
@@ -322,12 +159,23 @@ class bst {
     if (node == nullptr) {
       return;
     }
-    if (node == nullptr) {
-      return;
-    }
     postorder(node->left, iterators_order);
     postorder(node->right, iterators_order);
-    iterators_order.push_back(node->data);
+    iterators_order.push_back(node);
+  }
+
+  iterator minimum(iterator node) {
+    if (node->left == nullptr) {
+      return node;
+    }
+    return minimum(node->left);
+  }
+
+  iterator maximum(iterator node) {
+    if (node.right == nullptr) {
+      return node;
+    }
+    return maximum(node.right);
   }
 
  public:
@@ -349,13 +197,11 @@ class bst {
         end_{nullptr},
         rbegin_{nullptr},
         rend_{nullptr} {
-    other = allocator_.allocate(1);
-    allocator_.construct(other);
     std::vector<iterator> iterators_order;
     size_t i = 0;
-    preorder(this->root_, iterators_order);
+    preorder(other.root_, iterators_order);
     while (i < size_) {
-      other.insert(other.root_, iterators_order[++i]);
+      this->insert(this->root_, iterators_order[++i]);
     }
   }
 
@@ -384,23 +230,22 @@ class bst {
   }
 
   bst& operator=(const bst& other) {
-    bst new_bst = allocator_.allocate(1);
-    allocator_.construct(new_bst);
+    bst new_bst;
     new_bst.size_ = this->size_;
     std::vector<iterator> iterators_order;
     size_t i = 0;
-    preorder(other->root_, iterators_order);
+    preorder(other.root_, iterators_order);
     while (i < size_) {
       new_bst.insert(new_bst.root_, iterators_order[++i]);
     }
-    return &new_bst;
+    return new_bst;
   }
 
   ~bst() {
     std::vector<iterator> iterators_order;
     postorder(root_, iterators_order);
     for (size_type i = 0; i < size_; ++i) {
-      erase(root_, iterators_order[i]);
+      this->erase(this->root_, iterators_order[i]->value);
     }
     root_ = nullptr;
     size_ = 0;
@@ -426,7 +271,15 @@ class bst {
 
   bool operator!=(const bst& other) const { return !(this == other); }
 
-  void swap(bst& other) { std::swap(this->root_, other->root_); }
+  void swap(bst& other) {
+    std::swap(this->root_, other->root_);
+    std::swap(this->size_, other.size_);
+    std::swap(this->begin_, other.begin_);
+    std::swap(this->end_, other.end_);
+    std::swap(this->it_, other.it_);
+    std::swap(this->rbegin_, other.rbegin_);
+    std::swap(this->rend_, other.rend_);
+  }
 
   size_type size() const { return size_; }
 
@@ -434,22 +287,13 @@ class bst {
 
   bool empty() const { return size_ == 0; }
 
-  reference operator++() const { return ++it_; }
-
-  reference operator++(int) const { return it_++; }
-
-  reference operator--() const { return --it_; }
-
-  reference operator--(int) const { return it_--; }
-
   reference operator*() { return *it_; }
 
   iterator rbegin() const { return rbegin_; }
 
   iterator rend() const { return rend_; }
 
-  iterator insert(const_iterator node, const value_type& x,
-                  Compare comparator) {
+  iterator insert(iterator node, const value_type& x, Compare comparator) {
     if (node == nullptr) {
       ++size_;
       node = allocator_.allocate(1);
@@ -463,9 +307,9 @@ class bst {
       node->right = insert(node->right, x);
       node->right->parent = node;
     }
-    it_(size_, root_);
+    it_->initiliaze(this->size_, this->root_);
     begin_ = it_.iterators_order[0];
-    end_ = it_.iterators_order[size_ - 1].left;
+    end_ = it_.iterators_order[size_ - 1];
     rbegin_ = end_;
     rend_ = begin_;
     return node;
@@ -474,7 +318,7 @@ class bst {
   iterator insert(iterator i, iterator j) {
     iterator result;
     for (iterator current = i; current != j; ++current) {
-      result = this->insert(this->root_, *current);
+      result = this->insert(this->root_, current);
     }
     return result;
   }
@@ -495,37 +339,33 @@ class bst {
     other.clear();
   }
 
-  iterator erase(const_iterator node, const value_type& x) {
+  iterator erase(iterator node, const value_type& x) {
     if (node == nullptr) {
       return node;
     }
-    if (Compare()(x, node->value)) {
+    if (Compare{}(x, node->value)) {
       node->left = erase(node->left, x);
-    } else if (Compare()(node->value, x)) {
+    } else if (Compare{}(node->value, x)) {
       node->right = erase(node->right, x);
     } else {
       if (node->left == nullptr) {
         iterator current = node->right;
         current->parent = node->parent;
         --size_;
-        allocator_.destroy(node);
-        node = allocator_.deallocate(1);
         return current;
       } else if (node->right == nullptr) {
         iterator current = node->left;
         current->parent = node->parent;
-        allocator_.destroy(node);
-        node = allocator_.deallocate(1);
         --size_;
         return current;
       }
       iterator current = minimum(node->right);
       node->value = current->value;
-      node->right = erase(node.i_->right, current.i_->value);
+      node->right = erase(node->right, current->value);
     }
-    it_(size_, root_);
-    begin_ = it_.iterators_order[0];
-    end_ = it_.iterators_order[size_ - 1].left;
+    it_->initiliaze(this->size_, this->root_);
+    begin_ = it_->iterators_order[0];
+    end_ = it_->iterators_order[size_ - 1];
     rbegin_ = end_;
     rend_ = begin_;
     return node;
@@ -576,7 +416,7 @@ class bst {
   allocator_type get_allocator() { return allocator_; }
 };
 
-template <class T, class Traversal = InOrder, class Compare = std::less<T>,
+template <class T, Order Traversal = InOrder, class Compare = std::less<T>,
           class Allocator = std::allocator<T> >
 void swap(bst<T, Traversal>& lhs, bst<T, Traversal>& rhs) {
   lhs.swap(rhs);
